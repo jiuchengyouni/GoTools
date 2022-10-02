e7e064595ae1cc791ef2dbc95338af0e7206e517
hn hn <2125102013@stu.hqu.edu.cn>
2022-10-02 19:39:03 +0800

    计算微信公众平台的消息签名接口

--- utils/web/WxVerify.go
+++ utils/web/WxVerify.go
@@ -1,5 +1,29 @@
 package web
 
-type WxVerify struct{
-	
+import (
+	"crypto/sha1"
+	"encoding/hex"
+	"sort"
+)
+
+/**
+     * 用SHA1算法生成安全签名
+     * @param token 票据
+     * @param timestamp 时间戳
+     * @param nonce 随机字符串
+     * @return 安全签名
+     */
+func WxVerify(token string, timestamp string, nonce string) string {
+	//将token、timestamp、nonce三个参数进行字典序排序
+	var tempArray = []string{token, timestamp, nonce}
+	sort.Strings(tempArray)
+	//将三个参数字符串拼接成一个字符串进行sha1加密
+	var sha1String string = ""
+	for _, v := range tempArray {
+		sha1String += v
+	}
+	h := sha1.New()
+	h.Write([]byte(sha1String))
+	sha1String = hex.EncodeToString(h.Sum([]byte("")))
+	return sha1String
 }
